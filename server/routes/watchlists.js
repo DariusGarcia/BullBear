41cf4ad94f520fd15f406eb45b2b49fd0c270982
Darius Garcia <dariusgarcia888@gmail.com>
2022-09-09 20:54:07 -0700

    added watchlist controller to handle DB GET/POST requests

--- server/routes/watchlists.js
+++ server/routes/watchlists.js
@@ -1,41 +1,23 @@
 const express = require('express')
-
-const Watchlist = require('../models/watchlistModel')
+const {
+	getAllStocks,
+	createStock,
+	getSingleStock,
+	deleteStock,
+} = require('../controllers/watchlistController')
 
 const router = express.Router()
 
 // GET all stocks in watchlist
-router.get('/', (req, res) => {
-	res.json({ mssg: 'GET all stocks in watchlist' })
-})
+router.get('/', getAllStocks)
 
 // GET a single stock in watchlist
-router.get('/:id', (req, res) => {
-	res.json({ mssg: 'GET a single stock in watchlist' })
-})
+router.get('/:id', getSingleStock)
 
 // POST a single stock
-router.post('/', async (req, res) => {
-	const { ticker } = req.body
-
-	try {
-		const watchlist = await Watchlist.create({ ticker })
-		res.status(200).json(watchlist)
-	} catch (error) {
-		res.status(400).json({ error: error.message })
-	}
-})
+router.post('/', createStock)
 
 // DELETE a single stock
-router.delete('/:id', (req, res) => {
-	res.json({ mssg: 'DELETE a  new stock in watchlist' })
-})
-
-// UPDATE a single stock in watchlist'
-router.patch('/', (req, res) => {
-	res.json({ mssg: 'UPDATE the watchlist' })
-})
-
-router.get('/hello', () => {})
+router.delete('/:id', deleteStock)
 
 module.exports = router
