fa8cba0541e1e6e2851ee36e73faa4754d854989
Darius Garcia <dariusgarcia888@gmail.com>
2022-09-16 14:39:30 -0700

    v

--- server/index.js
+++ server/index.js
@@ -8,16 +8,6 @@ const express = require('express')
 // express app
 const app = express()
 app.use(cors())
-app.options('*', cors())
-
-app.all('*', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Content-Type,Content-Length, Authorization, Accept,X-Requested-With'
-	)
-	res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-})
 
 process.on('uncaughtException', function (err) {
 	console.error(err)
