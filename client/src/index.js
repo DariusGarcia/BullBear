a9820d87c1da9457500e1203c27839b73d3e82da
Darius Garcia <dariusgarcia888@gmail.com>
2022-09-22 21:06:25 -0700

    added scrolltotop functionality on every page rerender

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthContextProvider } from './Context/AuthContext'
 import { WatchListContextProvider } from './Context/watchlistContext'
+import ScrollToTop from './utils/ScrollToTop'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -13,9 +14,11 @@ root.render(
 		<AuthContextProvider>
 			<WatchListContextProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route path='*' element={<App />} />
-					</Routes>
+					<ScrollToTop>
+						<Routes>
+							<Route path='*' element={<App />} />
+						</Routes>
+					</ScrollToTop>
 				</BrowserRouter>
 			</WatchListContextProvider>
 		</AuthContextProvider>
